0496835f517e88ecef9fca0f0608b5949dd9b85e
Matt Kothe <matthew.ryan.kothe@gmail.com>
2022-01-28 09:21:13 -0500

    Deleted one line of tree, added range of numbers.

--- startfile.py
+++ startfile.py
@@ -6,9 +6,12 @@ print("---|---")
 print("---|---")
 print("---|---")
 print("---|---")
-print("---|---")
 print("-------")
 print("Cam changed something!")
 
 if __name__ == "__main__":
     print("funee")
+
+    nums = [x for x in range(1, 20)]
+
+    print(nums)
