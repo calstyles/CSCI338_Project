64ceb27619f60ca7fcd6dcdd27d0e28b8b1d1194
MatthewKothe <matthew.ryan.kothe@gmail.com>
2022-02-08 12:50:14 -0500

    Unindented main

--- src/__init__.py
+++ src/__init__.py
@@ -1,8 +1,8 @@
 
 def main():
-
+    pass
   
 
 # 'Main' function. Don't change or alter unless there's an issue.
-    if __name__ == "__main__":
-        main()
+if __name__ == "__main__":
+    main()
