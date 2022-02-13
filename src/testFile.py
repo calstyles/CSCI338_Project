59e57d7e4463e9d3ff34bcfc90ef28377ff94827
MatthewKothe <matthew.ryan.kothe@gmail.com>
2022-02-13 12:50:23 -0500

    Cleaned up the gui, changed name of testFile, and added a gitignore.

--- src/testFile.py
+++
@@ -1,5 +0,0 @@
-def apple_bottom_jeans(times):
-    for i in range(times):
-        new_num = pow(times, 2)
-        print("Shawty got them apple bottom jeans, boots with the fur. ")
-        print(new_num)
