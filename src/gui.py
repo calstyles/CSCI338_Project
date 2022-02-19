f82561054844e96b80e612430d9c3db59f1815e4
matthew <mkothe@unca.edu>
2022-02-19 09:09:47 -0500

    Code to see if torch is installed and if you have GPU capabilities

--- src/gui.py
+++ src/gui.py
@@ -1,30 +1,19 @@
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel
+from tkinter import *
+from tkinter import ttk
 
-# Only needed for access to command line arguments
-import sys
+"""
+def start_up_window():
+    _window = Tk()
+    _frm = ttk.Frame(_window, padding=10)
+    _frm.grid()
+    ttk.Label(_window, text="Software Engineering Project - Team3D").grid(column=0, row=0)
+    ttk.Button(_window, text="Quit", command=_window.destroy).grid(column=1, row=0)
+    _window.mainloop()
+"""
 
-# You need one (and only one) QApplication instance per application.
-# Pass in sys.argv to allow command line arguments for your app.
-# If you know you won't use command line arguments QApplication([]) works too.
-app = QApplication(sys.argv)
-
-# Create a Qt widget, which will be our window.
-window = QWidget()
-window.setWindowTitle('SWE Team3d')
-
-#First two parameters are where the window will be placed on the screen.
-#Second two parameters are the dimensions of the window.
-window.setGeometry(100, 100, 280, 80)
-window.move(60, 15)
-helloMsg = QLabel('<h1>Testing!</h1>', parent=window)
-helloMsg.move(60, 15)
-#quitButton = QWidget.
-window.show()
-
-
-# Start the event loop.
-sys.exit(app.exec_())
-
-
-# Your application won't reach here until you exit and the event
-# loop has stopped.
+root = Tk()
+frm = ttk.Frame(root, padding=10)
+frm.grid()
+ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
+ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
+root.mainloop()
--- src/machine_learning_playground.py
+++ src/machine_learning_playground.py
@@ -1,2 +1,5 @@
 import torch
 
+print('Your torch library is located here:\n', torch.version)
+print('Do you have GPU capabilites?\n', torch.cuda.is_available())
+
